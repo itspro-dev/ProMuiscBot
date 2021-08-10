df6ee758102aed624f9e63203b6af592cd212882
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2021-08-11 02:23:28 +0600

    Update userbotjoin.py

--- YuiHirasawaMusicBot/modules/userbotjoin.py
+++ YuiHirasawaMusicBot/modules/userbotjoin.py
@@ -36,7 +36,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @Rengoku_Kyujoro_Helper to your Group and try again</b>",
+            "\n\nOr manually add @animefansclubchat to your Group and try again</b>",
         )
         return
     await message.reply_text(
@@ -111,7 +111,7 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @Rengoku_Kyujoro_Helper to your Group and try again</b>",
+            "\n\nOr manually add @animefansclubchat to your Group and try again</b>",
         )
         return
     await message.reply_text(
