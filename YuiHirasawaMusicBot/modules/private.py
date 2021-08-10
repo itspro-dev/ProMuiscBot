9e8d8f25d75c6071e9a937750cb341ceeff37cb4
itspro-dev <84543009+itspro-dev@users.noreply.github.com>
2021-08-11 02:28:17 +0600

    Update private.py

--- YuiHirasawaMusicBot/modules/private.py
+++ YuiHirasawaMusicBot/modules/private.py
@@ -22,24 +22,16 @@ def _start(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "➕️ ADD MIZUHARA TO YOUR GROUP 🐣", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+                        "➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
                 [
                     InlineKeyboardButton(
-                        "📮 UPDATE CHANNEL", url=f"https://t.me/{UPDATES_CHANNEL}"), 
+                        "📲 Updates", url=f"https://t.me/{UPDATES_CHANNEL}"), 
                     InlineKeyboardButton(
-                        "🌟SUPPORT🌟", url=f"https://t.me/{SUPPORT_GROUP}")
-                ],
-                [
+                        "💬 Support", url=f"https://t.me/{SUPPORT_GROUP}")
+                ],[
                     InlineKeyboardButton(
-                        "🌟ANIME FANS CLUB🌟", url=f"https://t.me/animefansclubchat"), 
-                    InlineKeyboardButton(
-                        "FRIENDSHIP GROUP🌟", url=f"https://t.me/friendship_chatting")
-                ],
-                
-#                 [
-#                     InlineKeyboardButton(
-#                         "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
-#                 ]
+                         "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
+                ]
             ]
         ),
         reply_to_message_id=message.message_id
@@ -93,13 +85,12 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
         url = f"https://t.me/{SUPPORT_GROUP}"
         button = [
-            [InlineKeyboardButton("➕️ ADD MIZUHARA TO YOUR GROUP 🧚‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-            [InlineKeyboardButton(text = '📮 UPDATE CHANNEL', url=f"https://t.me/{UPDATES_CHANNEL}"),
-             InlineKeyboardButton(text = '🌟SUPPORT🌟', url=f"https://t.me/{SUPPORT_GROUP}")],
-            [InlineKeyboardButton(text = '🌟ANIME FANS CLUB🌟', url=f"https://t.me/animefansclubchat"),
-             InlineKeyboardButton(text = '🌟FRIENDSHIP GROUP🌟', url=f"https://t.me/friendship_chatting")],
-#             [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}")],
+            [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+            [InlineKeyboardButton(text = '📲 Updates', url=f"https://t.me/{UPDATES_CHANNEL}"),
+             InlineKeyboardButton(text = '💬 Support', url=f"https://t.me/{SUPPORT_GROUP}")],
+            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}")],
             [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
+        ]
     else:
         button = [
             [
